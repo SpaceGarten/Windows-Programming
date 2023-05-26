ffabec05aaeb00aad862dfbee6d35daa3b7557ba
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-05-25 17:35:14 -0700

    Add files via upload

---
+++ lab4-1/lab4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(234, 91);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(90, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 15);
+            label1.TabIndex = 1;
+            label1.Text = "vehicle speed in MPH:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(139, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Hours Travel:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(234, 137);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(143, 203);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(233, 139);
+            listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(182, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "calculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(301, 376);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ lab4-3/lab4-3/Form1.cs
@@ -0,0 +1,128 @@
+using System.Windows.Forms;
+
+namespace lab4_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean lightON = true;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lightON = !lightON;
+            flip();
+
+        }
+
+        private void flip()
+        {
+            if (lightON)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                label1.Text = "ON";
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+                label1.Text = "OFF";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int myVar = 99;
+            setToZero(ref myVar);
+            MessageBox.Show(myVar.ToString()); //99
+        }
+
+        private void setToZero(ref int number)
+        {
+            number = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+            if (lightON)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+        }*/
+    }
+}
+
+
+
+/*
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean lightON = true;
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            lightON = !lightON;
+            flip();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            flip();
+        }
+
+        private void flip()
+        {
+            if (lightON)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+        }
+    }
+}
+
+*/
