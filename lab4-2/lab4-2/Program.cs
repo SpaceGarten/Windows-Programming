a51e0e6a85e2feb5c0f4ad76a8b608d77ffd51d5
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-08-09 09:45:37 -0700

    Update Program.cs

--- lab4-2/lab4-2/Program.cs
+++ lab4-2/lab4-2/Program.cs
@@ -1,17 +1,36 @@
-namespace lab4_2
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
-    }
-}
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def display_prime_numbers(start):
+    print(f"Prime numbers greater than or equal to {start}:")
+    count = 0
+    num = start
+    while True:
+        if is_prime(num):
+            print(num, end=" ")
+            count += 1
+        if count == 10:  # Change this number to display a different count of prime numbers per line
+            print()       # Print a new line after displaying the specified count of prime numbers
+            count = 0
+        num += 1
+
+if __name__ == "__main__":
+    try:
+        n = int(input("Enter an integer greater than 1: "))
+        if n <= 1:
+            print("Please enter an integer greater than 1.")
+        else:
+            display_prime_numbers(n)
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
